b1c05a6fd0a5bef3d99ecf806ed7c535fcb87ac0
Adesh <aadeshgujar9689@gmail.com>
2026-07-27 13:09:12 +0530

    Add files via upload

---
+++ if_problem_stmts_below_avg.py
@@ -0,0 +1,38 @@
+#check_num is zero or non-zero
+# def zero_non_zero():
+#     x=int(input("Enter any number:"))
+#     if x==0:
+#         print("Given no. is a zero no.")
+#     else:
+#         print("Given no. is non-zero")
+# zero_non_zero()
+
+#find largest of two numbers
+# def largest():
+#     x=int(input("Enter first number:"))
+#     y=int(input("Enter another number:"))
+#     if x>y:
+#         print(f"{x} is largest")
+#     else:
+#         print(f"{y} is largest")
+# largest()
+
+# def pos_neg():
+#     o=int(input("Enter any number"))
+#     if o>0:
+#         print(f"{o} is a positive number")
+#     else:
+#         print(f"{o} is a negative number")
+# pos_neg()
+
+# def vowel_consonants():
+#     ch = input("Enter an alphabet:")
+#     if len(ch) ==1  and ch.isalpha():
+#         if ch in "aeiouAEIOU":
+#             print("vowel")
+#         else:
+#             print("consonant")
+#     else:
+#         print("Invalid input")
+# vowel_consonants()
+  
